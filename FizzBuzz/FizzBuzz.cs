f45e64bda8a6e82e0ce8c4d3211c3bca94f5fdab
Kira Stout <kstout@tokenex.com>
2025-12-11 16:53:52 -0500

    Refactor Convert method to use two booleans with single return
    - Replace multiple if-else with two boolean checks
    - Use isDivisibleBy3 and isDivisibleBy5 booleans
    - Build result string conditionally instead of multiple returns
    - Single return statement at the end
    - Add PSEUDOCODE.md with detailed explanation for new developers

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,16 +1,55 @@
-﻿namespace FizzBuzz;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FizzBuzz;
 
 public class FizzBuzz
 {
+    //CHALLLENGE: Implement the FizzBuzz logic as described below:
+    //FOR number from 1 to 100:
+    //IF(number is divisible by 15) THEN
+    //    PRINT "FizzBuzz"
+    // ELSE IF(number is divisible by 3) THEN
+    //    PRINT "Fizz"
+    // ELSE IF(number is divisible by 5) THEN
+    //     PRINT "Buzz"
+    // ELSE
+    // PRINT number
     public static string Convert(int number)
     {
-        if (number % 15 == 0)
-            return "FizzBuzz";
-        else if (number % 3 == 0)
-            return "Fizz";
-        else if (number % 5 == 0)
-            return "Buzz";
-        else
-            return number.ToString();
+        bool isDivisibleBy3 = number % 3 == 0;
+        bool isDivisibleBy5 = number % 5 == 0;
+
+        string result = "";
+
+        if (isDivisibleBy3)
+            result += "Fizz";
+
+        if (isDivisibleBy5)
+            result += "Buzz";
+
+        if (result == "")
+            result = number.ToString();
+
+        return result;
     }
+
+    // for 1 to 100
+    public static void RunFizzBuzz()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            // Reused Convert method for clean code
+            string result = Convert(i);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz.RunFizzBuzz();
+        }
+    }
+
 }
